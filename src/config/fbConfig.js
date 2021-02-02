b1def7e3beb1dfc63da65c96d61b5f29df1e4ad7
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2021-02-02 22:54:37 +0200

    npm install firebase add fbConfig and export function npm install react-redux-firebase redux-firestore

---
+++ src/config/fbConfig.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyDbtUMDnWhw8MPEMIetw4-jRa7p8egZgHI",
+  authDomain: "property24mock-bf46a.firebaseapp.com",
+  projectId: "property24mock-bf46a",
+  storageBucket: "property24mock-bf46a.appspot.com",
+  messagingSenderId: "713219223732",
+  appId: "1:713219223732:web:086047db8f20c6096a67e1",
+  measurementId: "G-81ZL19GNEX"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebase;
