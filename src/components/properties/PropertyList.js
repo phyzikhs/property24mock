00b379144accc626e37eb23c6ae57248e3131465
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2021-02-02 14:47:10 +0200

    Property summary from PropertySummary Property summary returns the summary of a property

--- src/components/properties/PropertyList.js
+++ src/components/properties/PropertyList.js
@@ -1,34 +1,17 @@
 import React from 'react'
+import PropertySummary from './PropertySummary'
 
 
 const PropertyList = () => {
   return (
     <div className="project-list section">
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title ">Project title</span>
-          <p>Posted by The Net Ninja</p>
-          <p className="grey-text">3rd September, 2am</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title ">Project title</span>
-          <p>Posted by The Net Ninja</p>
-          <p className="grey-text">3rd September, 2am</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title ">Project title</span>
-          <p>Posted by The Net Ninja</p>
-          <p className="grey-text">3rd September, 2am</p>
-        </div>
-      </div>
-      
+      <PropertySummary />
+      <PropertySummary />
+      <PropertySummary />
+      <PropertySummary />
+      <PropertySummary />
+      <PropertySummary />
+      <PropertySummary />
     </div>
   );
 }
