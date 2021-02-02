bacc9a48473ba17f6482c7e786bffd4dc62c0240
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2021-02-02 15:21:58 +0200

    Created Sign in and sign up forms Routers trigger forms from links and added route to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Dashboard from "./components/dashboard/Dashboard";
 import Navbar from "./components/layout/Navbar";
 import PropertyDetails from './components/properties/PropertyDetails'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
 
 
 class App extends Component {
@@ -14,6 +16,8 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Dashboard} />
             <Route path='/property/:id' component={PropertyDetails} />
+            <Route path='/login' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -4,8 +4,8 @@ const SignedOutLinks = () => {
 
   return (
     <ul className="right">
-      <li><NavLink to='/'>Login</NavLink></li>
-      <li><NavLink to='/'>Sign Up</NavLink></li>
+      <li><NavLink to='/login'>Login</NavLink></li>
+      <li><NavLink to='/signup'>Sign Up</NavLink></li>
     </ul>
   );
 }
