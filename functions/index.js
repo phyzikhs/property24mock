f8c68e6bac486f6d252506090b24858cd4680f62
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2021-02-03 22:08:01 +0200

    Created notification function that creates notification

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase)
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +9,21 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello to Zakes from Firebase!");
 });
+
+const createNotification = (notification => {
+  return admin.firestore().collection('notifications')
+    .add(notification)
+    .then(doc => console.log('Notification added', doc));
+});
+
+exports.propertyCreated = functions.firestore
+  .document('property/{propertyId}')
+  .onCreate(doc => {
+    const property = doc.data();
+    const notification = {
+      content: 'Added a new property',
+      user: `${property.authorFirstName} ${property.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    }
+    return createNotification(notification);
+  })
