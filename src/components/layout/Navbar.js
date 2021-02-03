362cb29471bede2ebedbb56915201b786f873439
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2021-02-03 11:43:34 +0200

    added firebaseReducer on rootReducer for authentication connected Navbar to map firebase.auth to props

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,17 +1,27 @@
+import { connect } from "react-redux"
 import { Link } from "react-router-dom"
 import SignedInLinks from "./SignedInLinks"
 import SignedOutLinks from "./SignedOutLinks"
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const {auth} = props; //, profile
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to='/' className='brand-logo'>property24</Link>
       </div>
-      <SignedInLinks />
-      <SignedOutLinks />
+      {links}
     </nav>
   )
 }
 
-export default Navbar;
+const mapStateToProps = (state) => {
+  // console.log(state);
+  return {
+    auth: state.firebase.auth,
+    // profile: state.firebase.profile
+  }
+}
+
+export default connect(mapStateToProps)(Navbar);
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import authReducer from './authReducer'
 import { combineReducers } from 'redux'
 import propertyReducer from './propertyReducer';
 import { firestoreReducer } from 'redux-firestore';
+import { firebaseReducer } from 'react-redux-firebase';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   property: propertyReducer,
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
+  firebase: firebaseReducer
 });
 
 export default rootReducer;
